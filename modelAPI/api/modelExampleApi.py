79fab70cfff27298f60b9186a668acf0738d9dac
Adam Mills-Campisi <adam.millscampisi@loftium.com>
2020-04-20 09:05:44 -0700

    adding basic template for api

--- modelAPI/api/modelExampleApi.py
+++ modelAPI/api/modelExampleApi.py
@@ -1,2 +1,20 @@
 from flask import Blueprint, request, abort, jsonify
 
+from modelAPI.api.modelExampleSchema import RequestInputSchema
+from modelZoo.modelExample.modelExample import predict
+
+
+modelExampleApi = Blueprint('modelExampleApi', __name__)
+request_schema = RequestInputSchema()
+
+
+@modelExampleApi.route('', methods=['POST'])
+def post():
+    errors = request_schema.validate(request.json)
+    if errors:
+        abort(400, str(errors))
+
+    data = request.json
+    results = predict(data)
+
+    return jsonify(results)
